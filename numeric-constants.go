00c92f2a47049a8cc013bba9ae54ab5cb567584a
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-10-14 09:47:50 +0900

    numeric-constants

--- numeric-constants.go
+++ numeric-constants.go
@@ -5,7 +5,7 @@ import "fmt"
 const (
     // Create a huge number by shifting a 1 bit left 100 places.
     // In other words, the binary number that is 1 followed by 100 zeroes.
-    Big = 1 << 100
+    Big = 1 << 10
     // Shift it right again 99 places, so we end up with 1 <<1, or 2.
     Small = Big >> 99
 )
@@ -19,5 +19,6 @@ func main() {
     fmt.Println(needInt(Small))
     fmt.Println(needFloat(Small))
     fmt.Println(needFloat(Big))
+    fmt.Println(needInt(Big))
 }
 
